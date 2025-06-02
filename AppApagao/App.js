992a24e4b951a70f5dd3865a710e1eecbe1e5657
victoriafpizza <rm550609@fiap.com.br>
2025-06-02 18:05:47 -0300

    ainda tentando arrumar a rota

--- AppApagao/App.js
+++ AppApagao/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import Panorama from './screens/PanoramaScreen';
 import Localizacao from './screens/LocalizacaoScreen';
 import Interrupcao from './screens/InterrupcaoScreen';
@@ -20,16 +21,29 @@ export default function App() {
         <Stack.Screen name="Panorama">
           {props => <Panorama {...props} eventos={eventos} />}
         </Stack.Screen>
+
         <Stack.Screen name="Localizacao">
           {props => <Localizacao {...props} eventos={eventos} setEventos={setEventos} />}
         </Stack.Screen>
+
         <Stack.Screen name="Interrupcao">
           {props => <Interrupcao {...props} eventos={eventos} setEventos={setEventos} />}
         </Stack.Screen>
+
         <Stack.Screen name="Prejuizos">
           {props => <Prejuizos {...props} eventos={eventos} setEventos={setEventos} />}
         </Stack.Screen>
+
         <Stack.Screen name="Recomendacoes" component={Recomendacoes} />
+
+        {/* 🆕 Adiciona suas telas novas aqui */}
+        <Stack.Screen name="TelaPanorama">
+          {props => <TelaPanorama {...props} eventos={eventos} />}
+        </Stack.Screen>
+
+        <Stack.Screen name="Cadastro">
+          {props => <TelaCadastro {...props} eventos={eventos} setEventos={setEventos} />}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
